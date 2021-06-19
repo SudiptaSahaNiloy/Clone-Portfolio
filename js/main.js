d939fe1708a12b8055bb9dd5433a2e594c449cb0
Sudipta Saha Niloy <niloy305968@gmail.com>
2021-06-19 17:59:31 +0600

    website developed

--- js/main.js
+++ js/main.js
@@ -22,16 +22,34 @@ $(document).ready(function () {
 
     // Owl Carousel
     $(".site-main .about-area .owl-carousel").owlCarousel({
-        loop:true, 
+        loop: true,
         autoplay: true,
         dots: true,
         responsive: {
-            0:{
+            0: {
                 items: 1
             },
-            544:{
+            544: {
                 items: 2
             }
         }
     });
+
+    // sticky nav menu
+    let nav_offset_top = $('.header_area').height() + 50;
+
+    function navbarFixed() {
+        if ($('.header_area').length) {
+            $(window).scroll(function () {
+                let scroll = $(window).scrollTop();
+                if (scroll >= nav_offset_top) {
+                    $('.header_area .main_menu').addClass('navbar_fixed');
+                } else {
+                    $('.header_area .main_menu').removeClass('navbar_fixed');
+                }
+            })
+        }
+    }
+
+    navbarFixed();
 });
